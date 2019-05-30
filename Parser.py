7b09a0ef067d9c2e7ed588490568ecdef1373665
Vitor Vinicius Beires Barbosa <vitor.vinicius@icloud.com>
2019-05-30 10:59:23 -0300

    Terminadas as 3 tasks, codigo pronto para revisão e aprimoramentos

--- Parser.py
+++ Parser.py
@@ -1,18 +1,18 @@
 import re
 import json
 
-# file = open(input('Digite o arquivo de entrada: '), 'r')
 file = open('games_teste.log', 'r')
 
 lines = file.readlines()
 
 games = []
+kills_by_means = []
 
 def newGame():
     games.append({
             'total_kills': 0,
             'players': [],
-            'kills': {}
+            'kills': {},
         })
 
 def substring(string, init, end):
@@ -23,6 +23,7 @@ for line in lines:
     if "InitGame:" in line: 
         newGame()
         gamesCount = gamesCount + 1
+        kills_by_means.append({})
     
     if "ClientUserinfoChanged:" in line: 
         player = substring(line, line.find('n\\')+2, line.find('\\t'))
@@ -39,6 +40,42 @@ for line in lines:
         killer = substring(line, re.search('\d:\s', line).span()[1], line.find(' killed'))
         games[gamesCount]['kills'][killer] += 1
         games[gamesCount]['total_kills'] += 1
+    
+    if 'by' in line:
+        mean_of_death = substring(line, re.search('by ', line).span()[1], len(line)-1)
+        if mean_of_death in kills_by_means[gamesCount]:
+            kills_by_means[gamesCount][mean_of_death] += 1
+        else:
+            kills_by_means[gamesCount][mean_of_death] = 1
 
 
-print(games)    
+
+# -------------- Task 1 --------------
+print('TASK 1')
+for i in range(0, len(games)):
+    print('game_' + str(i+1) +':', json.dumps(games[i], indent=4))
+
+# -------------- Task 2 --------------
+ranking = {}
+print('\nTASK 2')
+for i in range(0, len(games[i]['kills'])-1):
+    for key, value in games[i]['kills'].items():
+        if key in ranking:
+            ranking[key] += value
+        else:
+            ranking[key] = value
+
+print('RANKING:')
+for key, value in ranking.items():
+    print('PLAYER ' + str(key) + ' ' + str(value) + ' KILLS')
+
+for i in range(0, len(games[i]['kills'])-1):
+    print('\n Game ' + str(i+1))
+    for key, value in games[i]['kills'].items():
+        print('PLAYER ' + str(key) + ' ' + str(value) + ' KILLS')
+
+
+# -------------- Task 3 --------------
+print('\nCAUSAS DE MORTE:')
+for i in range(0, len(kills_by_means)):
+    print('game_' + str(i) + ': ', json.dumps(kills_by_means[i], indent=4) + '\n')
